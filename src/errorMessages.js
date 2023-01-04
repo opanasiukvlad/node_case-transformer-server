bc60df53912ea45c431030e2616cb9d618d200cc
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2023-01-04 19:31:43 +0200

    error message text fixed

--- src/errorMessages.js
+++ src/errorMessages.js
@@ -1,13 +1,16 @@
 const textIsMissingError = {
-  message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+  message: 'Text to convert is required. Correct request is: '
+  + '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
 };
 
 const toCaseIsMissingError = {
-  message: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+  message: '"toCase" query param is required. Correct request '
+ + 'is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
 };
 
 const toCaseValueError = {
-  message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
+  message: 'This case is not supported. Available '
+ + 'cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
 };
 
 module.exports = { textIsMissingError, toCaseIsMissingError, toCaseValueError };
